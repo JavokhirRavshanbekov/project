4a09c261819d94f15623c668edea1fb80f7d8975
Javohir <javokhir.ravshanbekov@gmail.com>
2025-06-27 10:55:29 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+"use strict";
+// let number = 10;
+// number = 20;
+// console.log(number);
+
+// const obj = {
+//   a: 50,
+// };
+
+// obj.a = 22;
+
+// console.log(obj.a);
+
+// let arr = [1, 2, 3];
+
+// // console.log(arr);
+
+// let obj = {
+//   name: "John",
+//   age: 20,
+//   city: "New York",
+
+//   arr: {
+//     name: "Lisa",
+//     age: 19,
+//     city: "Los Angeles",
+//   },
+// };
+// obj.arr.name = "Anna";
+// obj.age = 22;
+
+// console.log(obj);
+
+// const result = prompt("are you here?");
+// if (result == "yes") {
+//   document.body.style.backgroundColor = "green";
+// } else if (result == "no") {
+//   document.body.style.backgroundColor = "red";
+// } else {
+//   console.log("ok");
+// }
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const result = +prompt("How old are you?", "18");
+// console.log(result + 5);
+
+// const user = [];
+
+// user[0] = prompt("What is your name?");
+// user[1] = prompt("What is your surname?");
+// user[2] = prompt("How old are you?");
+
+// console.log("name: " + user[0] + " surname: " + user[1] + " age: " + user[2]);
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`);
+
+// let incr = 10,
+//   decr = 10;
+// // incr++;
+// // decr--;
+// console.log(++incr);
+// console.log(--decr);
